38f044c53c4e275090a2f8da5a67f6a848fbf897
Nicole <nicoledbanta@gmail.com>
2020-06-24 16:25:17 -0700

    Fix run scripts

--- airbnbScrape.js
+++ airbnbScrape.js
@@ -7,30 +7,18 @@ let rawHTML = 'https://www.airbnb.com/rooms/10469182?source_impression_id=p3_159
 
 rp(rawHTML)
   .then(function (rawHTML) {
-    // Wikipedia's code altered. Update if it changes again in the future. The mapping is based on 6/24/20
-    // const wikiUrls = [];
-    // for (let i = 8; i < 52; i++) {
-    //   wikiUrls.push($('b > a', html)[i].attribs.href);
-    // }
-    // console.log(wikiUrls);
-
-
-    /////////////////////////////////////
     let $ = cheerio.load(rawHTML);
 
     jsonframe($) // initializing the plugin
 
     let details = {
       "listing": "._14i3z6h",
+      "city": "._5twioja",
     }
 
     console.log($('body').scrape(details, { string: true }))
-
-    ///////////////////////////////////
 
   })
   .catch(function (err) {
     //handle error
   });
-
-
